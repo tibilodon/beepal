f842d28e86bf72df154516bf3f52758c79ff6af3
Vigh Tibor <tibilodondev@gmail.com>
2023-01-22 20:42:12 +0100

    -CARD/ dummy product pictures added

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Card from "@/components/Card";
 import styles from "@/styles/products.module.scss";
+import prod1 from "@/assets/prod.jpg";
+import prod2 from "@/assets/prod2.jpg";
+import prod3 from "@/assets/prod3.jpg";
 
 interface ProductsProps {
   props: any;
@@ -12,9 +15,9 @@ const Products: React.FC<ProductsProps> = () => {
       <div className={styles.wrap}>
         <h1>Products</h1>
         <div>
-          <Card path="/acacia"></Card>
-          <Card path="/colza"></Card>
-          <Card path="/mixed"></Card>
+          <Card src={prod1} path="/acacia"></Card>
+          <Card src={prod2} path="/colza"></Card>
+          <Card src={prod3} path="/mixed"></Card>
         </div>
       </div>
     </>
--- components/Card.tsx
+++ components/Card.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import Button from "./Button";
 import Link from "next/link";
 import styles from "@/styles/card.module.scss";
+import Image from "next/image";
 
 interface CardProps {
   path: string;
+  src: any;
 }
 
-const Card: React.FC<CardProps> = ({ path }) => {
+const Card: React.FC<CardProps> = ({ path, src }) => {
   return (
     <div className={styles.wrap}>
-      <h1>IMAGE</h1>
+      <div>
+        <Image alt="jar of honey" src={src} />
+      </div>
       <h1>title</h1>
       <h2>value of prod</h2>
       <Link href={path}>
