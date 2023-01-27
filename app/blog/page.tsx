6cfefce7d2c1560bb9cb2824dd055834d660ce15
Vigh Tibor <tibilodondev@gmail.com>
2023-01-27 11:15:29 +0100

    /blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,13 +1,22 @@
 import React from "react";
-import Card from "@/components/Card";
-import img from "@/assets/prod.jpg";
+import styles from "@/styles/blog.module.scss";
 
 const Blog = () => {
   return (
-    <div>
-      <h1>Blog</h1>
-      {/* <Card path="" src={img} /> */}
-    </div>
+    <>
+      <h1 className={styles.h1}>Blog</h1>
+      <div className={styles.wrap}>
+        <h4>Elkészült a honlapunk! </h4>
+        <p>
+          Újabb fejlődés történt Méhészetünk történetében, mégpedig az itt
+          látható honlap elkészülte. A termékkínálat most csak a mézeket fedi,
+          de az új szezonnal a termékeink is megújulnak. Amennyiben a
+          rendeléssel valamilyen probléma merülne fel, kérjük vegye fel a
+          kapcsolatot velünk itt.
+        </p>
+        <span>2023/02/01</span>
+      </div>
+    </>
   );
 };
 
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <h1>Contact</h1>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9244.195877820508!2d20.340501886000098!3d47.02045639649008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x474151046397cbd7%3A0xfd6b1f1c40469f04!2zTWFydGbFsSwgNTQzNQ!5e0!3m2!1sen!2shu!4v1674814288006!5m2!1sen!2shu"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+  );
 };
 
 export default Contact;
