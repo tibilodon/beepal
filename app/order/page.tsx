6abf33ff0e783d8ad894df4234fca529b45bd56b
Vigh Tibor <tibilodondev@gmail.com>
2023-02-07 12:38:19 +0100

    test v1

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { cookies } from "next/headers";
+import styles from "@/styles/order.module.scss";
+import Image from "next/image";
+import logo from "@/assets/beepalogo5.png";
+import Link from "next/link";
 
 const Done = () => {
   const nextCookies = cookies();
@@ -7,8 +11,22 @@ const Done = () => {
   const orderNo: number = Number(orderNoCookie?.value);
   return (
     <>
-      <h1>THANK YOU FOR YOUR ORDER, WE WILL GET IN TOUCH SOON...</h1>
-      <h1>ORDER NUMBER: {orderNo}</h1>
+      <div className={styles.wrap}>
+        <div className={styles.content}>
+          <h2>
+            Köszönjük <span>{orderNo}</span> számú rendelését!
+          </h2>
+          <p>
+            Hamarosan e-mail-ben értesítjük rendelésének állapotáról. Amennyiben
+            bármilyen kérdése van, kérjük lépjen velünk{" "}
+            <Link href={"/contact"}>
+              <span>kapcsolatba</span>
+            </Link>
+            .
+          </p>
+          <Image className={styles.img} alt="company logo" src={logo} />
+        </div>
+      </div>
     </>
   );
 };
